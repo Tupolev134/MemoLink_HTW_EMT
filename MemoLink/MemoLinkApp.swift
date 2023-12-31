537c3a43ac98f6224fdeb856b88ea7ce21806f5a
tupolev <jogloeckner@gmail.com>
2023-12-31 12:26:53 +0100

    Initial Commit

---
+++ MemoLink/MemoLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoLinkApp.swift
+//  MemoLink
+//
+//  Created by Jonas Glöckner on 31.12.23.
+//
+
+import SwiftUI
+
+@main
+struct MemoLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

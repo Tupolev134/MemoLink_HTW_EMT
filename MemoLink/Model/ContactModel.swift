c674f236528fe46c192e23280fbdcc520c5fd98f
carloausderwiesche <91966180+carloausderwiesche@users.noreply.github.com>
2024-01-02 22:01:30 +0100

    Update ContactModel.swift

--- MemoLink/Model/ContactModel.swift
+++ MemoLink/Model/ContactModel.swift
@@ -1,14 +1,13 @@
 import Foundation
 
 struct Contact: Identifiable {
-    var id: String        // eventuell NFC-Tag-ID
+    var id = UUID()        // eventuell NFC-Tag-ID
     var name: String
     var phoneNumber: String?
     var birthday: Date?
     var nfcTagIdentifier: String?
 
-    init(id: String, name: String, phoneNumber: String? = nil, email: String? = nil, birthday: Date? = nil, nfcTagIdentifier: String? = nil) {
-        self.id = id
+    init(name: String, phoneNumber: String? = nil, email: String? = nil, birthday: Date? = nil, nfcTagIdentifier: String? = nil) {
         self.name = name
         self.phoneNumber = phoneNumber
         self.birthday = birthday

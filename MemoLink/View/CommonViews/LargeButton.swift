7e68593f63f26a1dd9fd65a4f83ed8008394e00a
carloausderwiesche <91966180+carloausderwiesche@users.noreply.github.com>
2024-01-02 21:56:15 +0100

    created settings menu

---
+++ MemoLink/View/CommonViews/LargeButton.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+struct LargeButton: View {
+    var text: String
+    var action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            Text(text)
+                .font(.system(size: 32))
+                .bold()
+                .frame(maxWidth: .infinity)
+                .frame(height: 150)
+                .foregroundColor(Color("BG"))
+                .background(Color("MemoLinkBlue"))
+                .cornerRadius(10)
+        }
+        .padding()
+    }
+}
+
+struct LargeButton_Previews: PreviewProvider {
+    static var previews: some View {
+        LargeButton(text: "Large Button", action: {
+            // Action
+        })
+    }
+}
--- MemoLink/View/SettingsView/SettingsView.swift
+++ MemoLink/View/SettingsView/SettingsView.swift
@@ -1,16 +1,21 @@
-//
-//  SettingsView.swift
-//  MemoLink
-//
-//  Created by Carlo aus der Wiesche on 02.01.24.
-//
-
 import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
         VStack{
-            
+            Spacer()
+            LargeButton(text: "Contacts") {
+                //show all contacts
+            }
+            Spacer()
+            LargeButton(text: "New NFC Tag") {
+                //read nfc tag
+            }
+            Spacer()
+            LargeButton(text: "Delete NFC Tag") {
+                //delete nfc tag
+            }
+            Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color("BG").edgesIgnoringSafeArea(.all))

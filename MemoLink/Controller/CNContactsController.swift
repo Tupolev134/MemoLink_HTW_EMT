9adcc39d66a75633f7c710acead4c14b976559c1
carloausderwiesche <91966180+carloausderwiesche@users.noreply.github.com>
2024-01-23 18:41:36 +0100

    added addContactView

--- MemoLink/Controller/CNContactsController.swift
+++ MemoLink/Controller/CNContactsController.swift
@@ -5,7 +5,7 @@ import Contacts
 class CNContactsController {
     static let shared = CNContactsController()
     private let store = CNContactStore()
-
+    
     func loadContacts(completion: @escaping ([String: [CNContact]]) -> Void) {
         DispatchQueue.global(qos: .userInitiated).async {
             self.store.requestAccess(for: .contacts) { granted, error in
@@ -16,17 +16,17 @@ class CNContactsController {
                     }
                     return
                 }
-
+                
                 let keys = [CNContactGivenNameKey, CNContactFamilyNameKey] as [CNKeyDescriptor]
                 let request = CNContactFetchRequest(keysToFetch: keys)
                 request.sortOrder = .userDefault
-
+                
                 var newContacts = [String: [CNContact]]()
                 try? self.store.enumerateContacts(with: request) { contact, stop in
                     let key = String(contact.givenName.prefix(1))
                     newContacts[key, default: []].append(contact)
                 }
-
+                
                 DispatchQueue.main.async {
                     completion(newContacts)
                 }
@@ -35,25 +35,25 @@ class CNContactsController {
     }
     
     func fetchContactName(identifier: String, completion: @escaping (Result<String, Error>) -> Void) {
-            let keysToFetch: [CNKeyDescriptor] = [
-                CNContactGivenNameKey as CNKeyDescriptor,
-                CNContactFamilyNameKey as CNKeyDescriptor
-            ]
-
-            DispatchQueue.global(qos: .userInitiated).async {
-                do {
-                    let contact = try self.store.unifiedContact(withIdentifier: identifier, keysToFetch: keysToFetch)
-                    let name = contact.givenName + " " + contact.familyName
-                    DispatchQueue.main.async {
-                        completion(.success(name))
-                    }
-                } catch {
-                    DispatchQueue.main.async {
-                        completion(.failure(error))
-                    }
+        let keysToFetch: [CNKeyDescriptor] = [
+            CNContactGivenNameKey as CNKeyDescriptor,
+            CNContactFamilyNameKey as CNKeyDescriptor
+        ]
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                let contact = try self.store.unifiedContact(withIdentifier: identifier, keysToFetch: keysToFetch)
+                let name = contact.givenName + " " + contact.familyName
+                DispatchQueue.main.async {
+                    completion(.success(name))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
                 }
             }
         }
+    }
     
     func fetchContactDetails(identifier: String, completion: @escaping (Result<CNContact, Error>) -> Void) {
         let keysToFetch: [CNKeyDescriptor] = [
@@ -64,7 +64,7 @@ class CNContactsController {
             CNContactBirthdayKey as CNKeyDescriptor,
             CNContactImageDataKey as CNKeyDescriptor
         ]
-
+        
         DispatchQueue.global(qos: .userInitiated).async {
             do {
                 let contact = try self.store.unifiedContact(withIdentifier: identifier, keysToFetch: keysToFetch)
@@ -80,20 +80,57 @@ class CNContactsController {
     }
     
     func saveContactChanges(contact: CNMutableContact, completion: @escaping (Result<Void, Error>) -> Void) {
-            let saveRequest = CNSaveRequest()
-            saveRequest.update(contact)
-
-            DispatchQueue.global(qos: .userInitiated).async {
-                do {
-                    try self.store.execute(saveRequest)
-                    DispatchQueue.main.async {
-                        completion(.success(()))
-                    }
-                } catch {
-                    DispatchQueue.main.async {
-                        completion(.failure(error))
-                    }
+        let saveRequest = CNSaveRequest()
+        saveRequest.update(contact)
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                try self.store.execute(saveRequest)
+                DispatchQueue.main.async {
+                    completion(.success(()))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
                 }
             }
         }
+    }
+    
+    func addNewContact(firstName: String, lastName: String, phoneNumber: String, birthday: Date?, completion: @escaping (Result<Void, Error>) -> Void) {
+        let newContact = CNMutableContact()
+        newContact.givenName = firstName
+        newContact.familyName = lastName
+        
+        // Telefonnummer hinzufügen
+        if !phoneNumber.isEmpty {
+            let phoneValue = CNPhoneNumber(stringValue: phoneNumber)
+            let phoneLabel = CNLabeledValue(label: CNLabelPhoneNumberMobile, value: phoneValue)
+            newContact.phoneNumbers.append(phoneLabel)
+        }
+        
+        // Geburtsdatum hinzufügen, falls vorhanden
+        if let birthdayDate = birthday {
+            let calendar = Calendar.current
+            let dateComponents = calendar.dateComponents([.day, .month, .year], from: birthdayDate)
+            newContact.birthday = dateComponents
+        }
+        
+        // CNSaveRequest vorbereiten und den neuen Kontakt hinzufügen
+        let saveRequest = CNSaveRequest()
+        saveRequest.add(newContact, toContainerWithIdentifier: nil)
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                try self.store.execute(saveRequest)
+                DispatchQueue.main.async {
+                    completion(.success(()))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
 }
---
+++ MemoLink/View/ContactViews/AddContactView.swift
@@ -0,0 +1,67 @@
+import SwiftUI
+import Contacts
+
+struct AddContactView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var contactStorage = ContactStorageController.shared
+    @State private var firstName: String = ""
+    @State private var lastName: String = ""
+    @State private var phoneNumber: String = ""
+    @State private var birthday: Date = Date()
+    @State private var showingSaveAlert = false
+    @State private var saveAlertMessage: String = ""
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Name")) {
+                TextField("First name", text: $firstName)
+                TextField("Last name", text: $lastName)
+            }
+            
+            Section(header: Text("Phone")) {
+                TextField("Phone", text: $phoneNumber)
+            }
+            
+            Section(header: Text("Birthday")) {
+                DatePicker("Birthday", selection: $birthday, displayedComponents: .date)
+                    .datePickerStyle(WheelDatePickerStyle())
+            }
+            
+//            Section {
+//                Button("Save Contact") {
+//                    saveNewContact()
+//                }
+//            }
+        }
+        .navigationBarTitle("New Contact", displayMode: .inline)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: Button("Cancel") {
+            presentationMode.wrappedValue.dismiss()
+        }, trailing: Button("Save") {
+            saveNewContact()
+        })
+        .alert(isPresented: $showingSaveAlert) {
+            Alert(
+                title: Text("Save Contact"),
+                message: Text(saveAlertMessage),
+                dismissButton: .default(Text("OK")) {
+                    presentationMode.wrappedValue.dismiss()
+                }
+            )
+        }
+    }
+    
+    private func saveNewContact() {
+                let newContact = CNMutableContact()
+                newContact.givenName = firstName
+                newContact.familyName = lastName
+                newContact.phoneNumbers = [CNLabeledValue(label: CNLabelPhoneNumberMobile, value: CNPhoneNumber(stringValue: phoneNumber))]
+        
+                let birthdayComponents = Calendar.current.dateComponents([.day, .month, .year], from: birthday)
+                newContact.birthday = birthdayComponents
+        
+                // Speichern in CNContacts
+                
+    }
+    
+}
